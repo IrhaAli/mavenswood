367f3b022f1a1887f3349c0bcd2862c52e08d08c
Irha Ali <irhaali_97@hotmail.ca>
2024-02-13 12:43:30 -0500

    Header touch ups

--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,16 @@ function Header({ children, name, isFooter }) {
   const styles = {
     "margin-top": isFooter ? "50px" : "0px"
   }
+  const headerStyles = {
+    "margin": "40px",
+    "display": "flex",
+    "align-items": "center",
+    "justify-content": "center"
+  }
 
   return (
     <div className="header" style={styles}>
-      {name && <p>Welcome {name}</p>}
+      {name && <p style={headerStyles}>Welcome {name}</p>}
       {children}
     </div>
   );
